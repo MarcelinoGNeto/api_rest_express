87741c73ddb3102fce41860a6b3f949607d285f6
Marcelino Neto <mogt@gft.com>
2023-02-07 16:11:22 -0300

    aperfeiçoando os metodos

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import senha from "../../auth.js";
 
 mongoose.connect(
-  "mongodb+srv://marcelino:123@cluster0.9xif6gu.mongodb.net/marcelino-node"
+  `mongodb+srv://marcelino:${senha}@cluster0.9xif6gu.mongodb.net/marcelino-node`
 );
 
 let db = mongoose.connection;
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,14 @@
+//DEFINE O QUE VAI ACONTECER NA ROTA
+import express from "express";
+import LivroController from "../controllers/livroController.js";
+
+const router = express.Router();
+
+router
+  .get("/livros", LivroController.listarLivros)
+  .get("/livros/:id", LivroController.listarLivroPorId)
+  .post("/livros", LivroController.cadastrarLivro)
+  .put("/livros/:id", LivroController.atualizarLivro)
+  .delete("/livros/:id", LivroController.excluirLivro);
+
+export default router;
